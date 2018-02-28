d22d1ccf5ea1b69400ef7fd1c77bfc91b3406b4b
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-02-27 19:27:06 -0600

    add more things in pypdb.cpp

--- pypdb.cpp
+++ pypdb.cpp
@@ -9,9 +9,40 @@ using namespace boost::python;
 
 BOOST_PYTHON_MODULE(pypdb)
 {
-   class_<PDBDef>("PDBDef", init<const std::string&>());
-   class_<PDB>("PDB", init<const std::string&>())
-      .add_property("write2file", &PDB::write2file);
-   //class_<PDBDef>("PDBDef", init<std::string>());
+   enum_<PDBField>("PDBField")
+      .value("atomname", PDBField::atomname)
+      .value("resname", PDBField::resname)
+      .value("segname", PDBField::segname)
+      .value("atomtype", PDBField::atomtype)
+      .value("chainid", PDBField::chainid)
+      .value("resid", PDBField::resid)
+      .value("x", PDBField::x)
+      .value("y", PDBField::y)
+      .value("z", PDBField::z)
+      .value("occ", PDBField::occ)
+      .value("tempf", PDBField::tempf)
+      .value("unknown", PDBField::unknown);
+
+   void (PDBDef::*pushBack0) (PDBField, const std::string&) = &PDBDef::pushBack;
+   void (PDBDef::*pushBack1) (PDBField, const float) = &PDBDef::pushBack;
+   void (PDBDef::*pushBack2) (PDBField, const int) = &PDBDef::pushBack;
+   void (PDBDef::*pushBack3) (PDBField, const char) = &PDBDef::pushBack;
+   class_<PDBDef>("PDBDef")
+      .def(init<const std::string&>())
+      .def("push_back", pushBack0)
+      .def("push_back", pushBack1)
+      .def("push_back", pushBack2)
+      .def("push_back", pushBack3);
+
+   size_t (PDB::*reorderWater1)
+      (const PDBDef&, const PDBDef&, const PDBDef&) = &PDB::reorderWater;
+   class_<PDB>("PDB")
+      .def(init<const std::string&>())
+      .def("write2file", &PDB::write2file)
+      //.def("getAtomname", &PDB::getAtomname);
+      .add_property("x", &PDB::getX, &PDB::setX)
+      .def_readonly("natom", &PDB::getNatoms)
+      .def("set_atomname", &PDB::setAtomname)
+      .def("reorder_water", reorderWater1);
 }
 
